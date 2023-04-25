a97992495ee883927f7ef720691ace5247d32c6c
Ubuntu <ubuntu@ip-172-31-90-250.ec2.internal>
2023-04-25 19:50:59 +0000

    Drupal distribuido

--- Drupal/setup.sh
+++ Drupal/setup.sh
@@ -1,2 +1,12 @@
 apt install nfs-common
 mkdir /home/ubuntu/nfs
+mount -v 100.24.21.3:/ /home/ubuntu/nfs
+mkdir /home/ubuntu/nfs/modules
+mkdir /home/ubuntu/nfs/profiles
+mkdir /home/ubuntu/nfs/themes
+mkdir /home/ubuntu/nfs/sites
+mkdir /home/ubuntu/nfs/sites/default
+mkdir /home/ubuntu/nfs/sites/default/files
+cp ./default.settings.php /home/ubuntu/nfs/sites/default/default.settings.php
+cp ./default.settings.php /home/ubuntu/nfs/sites/default/settings.php
+chmod -R 777 /home/ubuntu/nfs
