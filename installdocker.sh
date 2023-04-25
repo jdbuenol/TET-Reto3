4d9b97a64e1d304ef03b620fd1688e092fc904db
jdbuenol <jdbuenol@eafit.edu.co>
2023-04-25 19:57:59 -0400

    corregido error en installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -3,7 +3,7 @@ apt-get install \
     ca-certificates \
     curl \
     gnupg
-mkdir -m 0755 -p /etc/apt/keyrings
+mkdir -m 755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
